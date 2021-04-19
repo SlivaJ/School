312110b02d53d1a7a637ed702d6f5e44cbbd4b0b
John <jcsliva1999@gmail.com>
2021-04-19 02:06:05 -0400

    Fixed passing
    Pretty sure I have everything worked out. strings, chars, and ints are all being passed between the threads as they should be and now I have to add the counting functionality and make sure it works on one thread before setting up the multiple thread case parts

--- CS/OSandNetworking/Homework/HW5_Threads/Threads.c
+++ CS/OSandNetworking/Homework/HW5_Threads/Threads.c
@@ -20,23 +20,21 @@ Homework Label: Threading
 #define BUFFER1 = 50;
 #define BUFFER2 = 50;
 
-typedef struct pass_values{
-//values to pass to thread
-char str[10000];
-char search_value[1];
-int char_count;
+typedef struct pass_values
+{
+    //values to pass to thread
+    char str[10000];
+    char search_value;
+    int char_count;
 
-}pass_values;
+} pass_values;
 //function counts the instances of a character in a section of a string.
 void *thread_function(void *arg)
 {
-    pass_values *dataset = (void*)arg;
-    printf("passed data in thread:\n string: %s\n search char: %s\n char count: %d\n", dataset->str,dataset->search_value,dataset->char_count);
-    // int *char_count = (void*)arg;
+    pass_values *dataset = (void *)arg;
+    printf("passed data in thread:\n string: %s\n search char: %c\n char count: %d\n", dataset->str, dataset->search_value, dataset->char_count);
     char str[1000];
-    //strcpy(str, (void *)arg);
-    // int *char_count = malloc(sizeof(int));
-    // *char_count = 10;
+
     int i = 0;
     //printf("string in thread:\n%s\n", str);
 
@@ -55,9 +53,9 @@ void *thread_function(void *arg)
 int main(int argc, char *argv[])
 {
     //vars from the input
-    char search_char[1]; //input char
+    char search_char; //input char
     char input_file[25]; //name of the file to be tested
-    char threadCountStr[3];
+    char threadCountStr;
     int thread_count = 5;
 
     //input handling
@@ -65,18 +63,19 @@ int main(int argc, char *argv[])
     printf("File name: ");
     scanf("%s", input_file);
     fflush(stdin);
-
     //char to look for
     printf("Char: ");
-    scanf("%s", search_char);
-    fflush(stdin);
+    scanf("%s", &search_char);
+    
+    // printf("char input:\n 0: %s\n 1:%s \n",&search_char[0],&search_char[1]);
+    // fflush(stdin);
 
     //number of threads
     while (thread_count > 4 || thread_count < 1)
     {
         printf("Thread Count (1-4): ");
-        scanf("%s", threadCountStr);
-        thread_count = atoi(threadCountStr);
+        scanf("%s", &threadCountStr);
+        thread_count = atoi(&threadCountStr);
         if (thread_count > 4 || thread_count < 1)
         {
             printf("Thread count must range from 1-4\n");
@@ -84,7 +83,7 @@ int main(int argc, char *argv[])
         fflush(stdin);
     }
 
-    printf("input data:\nfile: %s\nchar: %s\nthreads: %d\n", input_file, search_char, thread_count);
+    printf("input data:\nfile: %s\nchar: %c\nthreads: %d\n", input_file, search_char, thread_count);
 
     //string
     char str[10000];
@@ -96,8 +95,8 @@ int main(int argc, char *argv[])
     case 1: // counting with one thread
     {
         pass_values data_set;
-        strcpy(data_set.str,str);
-        strcpy(data_set.search_value,search_char);
+        strcpy(data_set.str, str);
+        data_set.search_value = search_char;
         int *count_val;
         pthread_t thread1;
         pthread_create(&thread1, NULL, thread_function, &data_set);
