ccea1923a4fb38db06616082a95216a5bbb7122a
John <jcsliva1999@gmial.com>
2021-04-26 18:25:44 -0400

    File update
    Added a few files and basic structure to scheduling homework

---
+++ CS/OSandNetworking/Homework/HW6_Scheduling/Code_Timer_mod.c
@@ -0,0 +1,97 @@
+/*
+name: John Sliva
+course: CS 330 Operating Systems
+date:
+Homework Label: Code Timer
+Description: Program will be used to view execution times.
+*/
+//libs used
+#include <stdio.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+//struct defined and renamed
+typedef struct time
+{
+    long seconds;
+    long uSeconds;
+} timeVal;
+
+//functions
+timeVal startTimer();
+timeVal endTimer();
+void timedCode();
+void printTime();
+
+//function returns the time.
+timeVal startTimer()
+{
+    //vars
+    struct timeval current_time;
+    timeVal startTime;
+    //getting time values
+    gettimeofday(&current_time, NULL);
+    startTime.seconds = (current_time.tv_sec);
+    startTime.uSeconds = (current_time.tv_usec);
+    return startTime;
+}
+//function returns the time.
+timeVal endTimer()
+{
+    //vars
+    struct timeval current_time;
+    timeVal endTime;
+    //getting time values
+    gettimeofday(&current_time, NULL);
+    endTime.seconds = (current_time.tv_sec);
+    endTime.uSeconds = (current_time.tv_usec);
+    return endTime;
+}
+
+//function calculates and outputs the results
+void printTime(timeVal start_Time, timeVal end_Time)
+{
+    //vars
+    long startSeconds,
+        startuSeconds,
+        resultCalc,
+        endSeconds,
+        enduSeconds,
+        enduSecondsVal,
+        resultSeconds,
+        resultuSeconds;
+    timeVal resultTime;
+    //setting values from input
+    startSeconds = start_Time.seconds;
+    startuSeconds = start_Time.uSeconds;
+    endSeconds = end_Time.seconds;
+    enduSeconds = end_Time.uSeconds;
+    enduSecondsVal = enduSeconds;
+    // condition test (End uSecond value is smaller than Start uSecond value)
+    /*  // testing special condition specifically
+    startSeconds = 3;
+    startuSeconds = 999999;
+    endSeconds = 4;
+    enduSeconds = 1;
+    enduSecondsVal = enduSeconds; */
+    //Calculate results
+    //condition fix (if usec of end time is smaller than usec of start then 1 min hasnt passed)
+    resultSeconds = endSeconds - startSeconds;
+    if (startuSeconds > enduSeconds)
+    {
+        resultSeconds = resultSeconds - 1;
+        enduSeconds = enduSeconds + 1000000;
+    }
+
+    resultuSeconds = enduSeconds - startuSeconds;
+    printf("The program started at Seconds:%ld uSeconds:%ld\n", startSeconds, startuSeconds);
+    printf("The program ended at Seconds:%ld uSeconds:%ld\n", endSeconds, enduSecondsVal);
+    printf("Time required to run Seconds:%ld uSeconds:%ld\n", resultSeconds, resultuSeconds);
+}
+
+// function runs the code that will be timed by this program.
+void timedCode()
+{
+    printf("This is the code that will be timed.\n");
+    //sleep(10);
+}
---
+++ CS/OSandNetworking/Homework/HW6_Scheduling/Scheduling.c
@@ -0,0 +1,42 @@
+/*
+Name: John Sliva
+Course: CS330: Operating Systems
+Date: 4/26/2021
+Homework Label: Scheduling
+//Using nice levels to change scheduling 
+//Includes Pthreads 
+//Includes Forking
+//Uses modified Code Timer from assignment two to display time 
+//******************************************************************************************
+//The threading code from HW5_Threads_v2 has been modified to have static values for inputs.
+//It will have 4 threads, and search for the char x, The input file is also included "" 
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+
+#define _GNU_Source
+/*
+//incriment value to count to 1,000,000,000
+//
+*/
+void *thread_function(void* arg){
+
+
+    return NULL;
+}
+/*
+
+*/
+void fork_function(){
+
+}
+/*
+//creates 2 forks   1:high nice 2:low nice
+//creates 2 threads 1:high nice 2:low nice
+*/
+int main (){
+
+}
