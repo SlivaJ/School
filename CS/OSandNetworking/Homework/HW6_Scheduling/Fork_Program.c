5cb234329a4aadfd3de3c0746606eb38e66c2c59
John <jcsliva1999@gmail.com>
2021-04-27 18:07:05 -0400

    function update
    confirmed that the fork and thread just need to be timed. It doesn't matter what they do. "They could even just wait()."

---
+++ CS/OSandNetworking/Homework/HW6_Scheduling/Fork_Program.c
@@ -0,0 +1,43 @@
+/*
+name: John Sliva
+course: CS 330 Operating Systems
+Homework Label: repurposed Basic Forking 
+Description: The output of this program, when properly implemented, will exemplify the unpredictable order in which the processes are scheduled to run.
+//changed function so that it would be able to be called from a seperate file.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+void forking(void)
+{
+    //process markers
+    char process[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    //perform exactly 12 "runs"
+    for (int runs = 0; runs < 12; runs++)
+    {
+        //create 26 child proecesses
+        for (int childProcess = 0; childProcess < 26; childProcess++)
+        {
+            pid_t mypid = fork();
+            if (mypid == 0)
+            {
+                // putchar(process[childProcess]);
+                exit(0);
+            }
+            else if (mypid < 0)
+            {
+                perror("Failed to fork");
+            }
+        }
+        while (wait(NULL) != -1)
+        {
+            /*waiting for loops*/
+        }
+        // printf("\n");
+    }
+    
+}
--- CS/OSandNetworking/Homework/HW6_Scheduling/Scheduling.c
+++ CS/OSandNetworking/Homework/HW6_Scheduling/Scheduling.c
@@ -15,28 +15,68 @@ Homework Label: Scheduling
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
+#include "Code_Timer_mod.c"
 
 #define _GNU_Source
+
 /*
 //incriment value to count to 1,000,000,000
 //
 */
-void *thread_function(void* arg){
-
-
+void *thread_function(void *arg)
+{
+    int i;
+    volatile int count_result;
+    int count_number = 1000000000;
+    for (i = 0; i < count_number; i++)
+    {
+        count_result = i;
+    }
     return NULL;
 }
 /*
-
+//incriment value to count to 1,000,000,000
+//
 */
-void fork_function(){
-
+void process_function()
+{
+    printf("This is the function task.(2)\n");
+    // int i;
+    // volatile int count_result;
+    // int count_number = 1000000000;
+    // for (i = 0; i < count_number; i++)
+    // {
+    //     count_result = i;
+    // }
+    
 }
 /*
 //creates 2 forks   1:high nice 2:low nice
 //creates 2 threads 1:high nice 2:low nice
 */
-int main (){
+int main()
+{
+    printf("main start(0)\n");
+    // pid_t pid = fork();
+    int pid = fork();
+    
+    
+    if(pid==-1){
+        perror("failed to fork\n");
+    }
+    if(pid==0){
+        printf("The child will now start the timer and run the process function.(1)\n");
+        process_function();
+    }
+    if(pid!=0){
+        wait(NULL);
+        printf("the main function will wait for the child and end the timer.(3)\n");
+        
+        printf("timer end(4)\n");
+    }
 
 }
