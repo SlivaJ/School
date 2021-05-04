4243a78870f2539adc9bd4dbd796ea0ac86722c8
John <jcsliva1999@gmial.com>
2021-05-04 16:02:27 -0400

    adding a few files
    nothing important

--- CS/OSandNetworking/Homework/HW6_Scheduling/Scheduling.c
+++ CS/OSandNetworking/Homework/HW6_Scheduling/Scheduling.c
@@ -18,65 +18,125 @@ Homework Label: Scheduling
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/resource.h>
 
 #include "Code_Timer_mod.c"
 
 #define _GNU_Source
-
 /*
-//incriment value to count to 1,000,000,000
-//
+//global vars
+*/
+int sleep_time = 5;
+/*
+//Waits for a few seconds to simulate doing work 
 */
 void *thread_function(void *arg)
 {
-    int i;
-    volatile int count_result;
-    int count_number = 1000000000;
-    for (i = 0; i < count_number; i++)
-    {
-        count_result = i;
+    
+    int big_number = 1000000;
+    volatile long sigma = 0;
+    // printf("This is in the thread.\n");
+    for(int i=0;i<big_number;i++){
+        sigma = sigma+i;  
     }
+    // sleep(sleep_time);
     return NULL;
 }
 /*
-//incriment value to count to 1,000,000,000
-//
+//Waits for a few seconds to simulate doing work 
 */
 void process_function()
 {
-    printf("This is the function task.(2)\n");
-    // int i;
-    // volatile int count_result;
-    // int count_number = 1000000000;
-    // for (i = 0; i < count_number; i++)
-    // {
-    //     count_result = i;
-    // }
-    
+    int big_number = 1000000;
+    for(int j=0;j<1000;j++){
+    volatile long sigma = 0;
+    // printf("This is in the thread.\n");
+    for(int i=0;i<big_number;i++){
+        sigma = sigma+i;  
+    }
+    }
+    // sleep(sleep_time);
 }
 /*
-//creates 2 forks   1:high nice 2:low nice
-//creates 2 threads 1:high nice 2:low nice
+//creates 2 threads 1:high nice 2:no nice
+//creates 2 forks   1:high nice 2:no nice
 */
 int main()
 {
     printf("main start(0)\n");
-    // pid_t pid = fork();
+    
+    /*
+    //timers for the program 
+    //these will be used for both threads and forks
+    */
+    timeVal start_no_nice, end_no_nice;
+    timeVal start_nice, end_nice;
+    /*
+    //Start off with the two threads
+    //thread one will change the nice
+   */
+    pthread_t nice_thread, not_nice_thread;
+    //******************************************************
+    //no nice
+    //******************************************************
+    start_no_nice = startTimer();
+    pthread_create(&nice_thread, NULL, thread_function, NULL);
+    pthread_join(nice_thread, NULL);
+    end_no_nice = endTimer();
+    
+    //******************************************************
+    //nice level increased
+    //******************************************************
+    sleep(sleep_time);
+    start_nice = startTimer();
+    nice(-10);
+    pthread_create(&not_nice_thread, NULL, thread_function, NULL);
+    pthread_join(not_nice_thread, NULL);
+    end_nice = endTimer();
+    printf("***************************************This thread is timed with no nice:***************************************\n");
+    printTime(start_no_nice, end_no_nice);
+    printf("***************************************This thread is timed with low nice***************************************\n");
+    printTime(start_nice, end_nice);
+    //******************************************************
+    //Threading time confirmed above: Below is for forking
+    //******************************************************
+    
+    printf("\n\n***************************************Below is forking***************************************\n\n");
+    // fork program
+    // child will split off and do the function task with a lower nice value.
+    // main will wait for child then end the timer. 
+    // nice will be reverted and main will call process function again and time it
+    //reset nice
+    nice(0);
+    
+    
     int pid = fork();
-    
-    
-    if(pid==-1){
+
+    if (pid == -1)
+    {
         perror("failed to fork\n");
     }
-    if(pid==0){
-        printf("The child will now start the timer and run the process function.(1)\n");
+    if (pid == 0)
+    {
+        nice(-19);
+        start_nice = startTimer();
         process_function();
+        end_nice = endTimer();
     }
-    if(pid!=0){
+    if (pid > 0)
+    {
         wait(NULL);
-        printf("the main function will wait for the child and end the timer.(3)\n");
         
-        printf("timer end(4)\n");
+        //reset nice
+        nice(0);
+        start_no_nice = startTimer();
+        process_function();
+        end_no_nice = endTimer();
+        printf("***************************************This process is timed with no nice:***************************************\n");
+        printTime(start_no_nice, end_no_nice);
+        printf("***************************************This process is timed with low nice:***************************************\n");
+        printTime(start_nice,end_nice);
     }
-
+    
+   return 0;
 }
